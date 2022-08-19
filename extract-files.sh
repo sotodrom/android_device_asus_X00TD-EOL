d867a29d86181430b4fed79216cc85137971eca9
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:39:47 +0200

    X00TD: Use libhidlbase from vndk v32 for libvendor.goodix.hardware.fingerprint@1.0.so
        * _ZN7android8hardware7details17gBnConstructorMapE and _ZN7android8hardware7details17gBsConstructorMapE have been removed on Android 13
    
    Change-Id: If7e0601d91c241e9ce39eeface0ac9156be30eac
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -79,6 +79,11 @@ function blob_fixup() {
         grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
         ;;
 
+        # Use VNDK 32 libhidlbase
+        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so)
+        "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
+        ;;
+
     esac
 }
 
