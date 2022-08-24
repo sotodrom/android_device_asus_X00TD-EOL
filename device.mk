f1398b4a9c842d3f77a05fd84382f895fd99ff5c
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    X00TD: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: dotkit <dotkit@electrowizard.me>

--- device.mk
+++ device.mk
@@ -8,6 +8,8 @@
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
 PRODUCT_SHIPPING_API_LEVEL := 27
 
