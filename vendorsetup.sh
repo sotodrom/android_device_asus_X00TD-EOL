1390910e1059c4291f232f688355aae45f9c6fb2
Meisie Widuri <ewprjkt@proton.me>
2024-06-05 01:35:45 +0700

    X00TD: Track msm8998 from our org

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,6 +7,13 @@ git clone --depth=1 https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 14-
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus_X00TD -b udc-4.4 vendor/asus
 
+rm -rf hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_audio -b 14 hardware/qcom-caf/msm8998/audio
+git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_display -b 14 hardware/qcom-caf/msm8998/display
+git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_media -b 14 hardware/qcom-caf/msm8998/media
+
 rm -rf vendor/lineage-priv
 git clone https://github.com/Tiktodz/vendor -b 14 kntl && cp -R kntl/* vendor/ && rm -rf kntl
 
